b1bce4d20ed1fad66045c20b9a89aa4a23df07ed
David Gragnolati <Dgragnolati@Davids-MacBook-Pro-2.local>
2020-04-15 23:44:17 -0700

    fixed number of pins, and fixed the copy and pase error duhhh

--- Keep_em_green.py
+++ Keep_em_green.py
@@ -5,9 +5,9 @@ import RPi.GPIO as GPIO
 # set shit up
 GPIO.setmode(GPIO.BOARD)
 
-#set Pump #4 (AKA WHITE LED (Schematic)
+#set Pump #16 (AKA Trigger (Schematic)
 Pump_Pin = 16
-#set Lights #17 (AKA BLUE LED (Schematic)
+#set Lights #18 (AKA Exot (Schematic)
 Lamp_Pin = 18
 # SET outputs
 GPIO.setup(Pump_Pin, GPIO.OUT)
@@ -35,11 +35,11 @@ GPIO.setup(Lamp_Pin, GPIO.OUT)
 #test script
 
 GPIO.output(Pump_Pin, GPIO.HIGH)
-GPIO.output(Pump_Pin, GPIO.HIGH)
+GPIO.output(Lamp_Pin, GPIO.HIGH)
 
 time.sleep(5)
 
 GPIO.output(Pump_Pin, GPIO.LOW)
-GPIO.output(Pump_Pin, GPIO.LOW)
+GPIO.output(Lamp_Pin, GPIO.LOW)
 
 GPIO.cleanup()
