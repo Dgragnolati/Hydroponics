91a3884c91d8dbbe08f6ee16f78cbba0bf6102f4
David Gragnolati <Dgragnolati@Davids-MacBook-Pro-2.local>
2020-04-26 14:14:49 -0700

    Adding Water only controls

---
+++ run_water.py
@@ -0,0 +1,19 @@
+#Function
+from datetime import datetime
+import RPi.GPIO as GPIO
+import time
+# set up GPIO
+GPIO.setmode(GPIO.BOARD)
+#turn specific controls (relays) on for a set duration and then turn off
+def control_on (pin,duration):
+    print("setting up GPIO Pin")
+    GPIO.setup(pin, GPIO.OUT)
+    print("turning on the control for pin " + str(pin))
+    GPIO.output(pin, GPIO.HIGH)
+    print("waiting for " + str(duration))
+    time.sleep(duration)
+    print("turn off the contorl")
+    GPIO.output(pin, GPIO.LOW)
+    GPIO.cleanup()
+
+contorl_on(16,600)
